e6bbd573066f0f5d2a96d29727a477f4f9de36d9
Abdelaziz ELOUAHAB <60048840+MrAbdelaziz@users.noreply.github.com>
2021-05-22 14:29:33 +0100

    Update watermark.py

--- watermark.py
+++ watermark.py
@@ -25,9 +25,10 @@ files = []
 for r, d, f in os.walk(path):
     for file in f:
         if '.jpg' in file:
-            id = re.findall(r'\d+', file)
+            files.append(file)
+         """ select certin images id = re.findall(r'\d+', file)
             if int(id[0]) >= 12:
-                files.append(file)
+                files.append(file)"""
 
 for f in files:
     print(f)
