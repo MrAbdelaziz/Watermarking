627658012f8a43a6ed1fd44d84287511dd4a2e8e
MrAbdelaziz <60048840+MrAbdelaziz@users.noreply.github.com>
2021-11-25 17:44:04 +0100

    Update watermark.py

--- watermark.py
+++ watermark.py
@@ -29,7 +29,21 @@ for r, d, f in os.walk(path):
          """ select certin images id = re.findall(r'\d+', file)
             if int(id[0]) >= 12:
                 files.append(file)"""
-
+   
 for f in files:
     print(f)
     subprocess.call(f"magick composite -dissolve 10% -gravity center logo.png {f} -resize 2480x3508 {f}")
+   
+   
+   """
+   uncomment for pdf 
+   for r, d, f in os.walk(path):
+    for file in f:
+        if '.pdf' in file:
+            files.append(file)
+
+for f in files:
+    print(f)
+    subprocess.call(f"magick composite -quality 100 -gravity East -density 400 -dissolve 80% logo.png  -geometry +100+20 {f} {f}")"""
+   
+   
